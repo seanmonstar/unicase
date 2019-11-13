889d4761a67f7f61f950e98ab7f8ad5a60001039
Jeremy A. Kolb <jkolb@ara.com>
2019-11-13 09:54:23 -0500

    Use autocfg instead of version_check

--- build.rs
+++ build.rs
@@ -1,23 +1,23 @@
-extern crate version_check as rustc;
+extern crate autocfg;
 
 fn main() {
-    if is_rustc_at_least("1.5.0") {
-        println!("cargo:rustc-cfg=__unicase__iter_cmp");
+    autocfg::rerun_path(file!());
+
+    let ac = autocfg::new();
+
+    if ac.probe_rustc_version(1, 5) {
+        autocfg::emit("__unicase__iter_cmp");
     }
 
-    if is_rustc_at_least("1.13.0") {
-        println!("cargo:rustc-cfg=__unicase__default_hasher");
+    if ac.probe_rustc_version(1, 13) {
+        autocfg::emit("__unicase__default_hasher");
     }
 
-    if is_rustc_at_least("1.31.0") {
-        println!("cargo:rustc-cfg=__unicase__const_fns");
+    if ac.probe_rustc_version(1, 31) {
+        autocfg::emit("__unicase__const_fns");
     }
 
-    if is_rustc_at_least("1.36.0") {
-        println!("cargo:rustc-cfg=__unicase__core_and_alloc");
+    if ac.probe_rustc_version(1, 36) {
+        autocfg::emit("__unicase__core_and_alloc");
     }
-}
-
-fn is_rustc_at_least(v: &str) -> bool {
-    rustc::is_min_version(v).unwrap_or(true)
 }
