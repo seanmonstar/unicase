a09db6425f9b87587f9f6b095df8a479c6c58cc2
Jules Bertholet <julesbertholet@quoi.xyz>
2026-06-15 11:42:46 -0400

    Miscellaneous code cleanups
    - Factor `self.0.as_ref().chars().flat_map(lookup)`
      into a helper method
    - Don't manually reimplement `Iterator::eq`
      (the MSRV is new enough to include it now)
    - Use `encode_utf8` from `core` instead of re-implementing it
    - Compare `u8` iterators instead of `char` in `Ascii::cmp`
      (avoid UTF-8 decoding overhead)

--- src/ascii.rs
+++ src/ascii.rs
@@ -53,8 +53,8 @@ impl<T: AsRef<str>> PartialOrd for Ascii<T> {
 impl<T: AsRef<str>> Ord for Ascii<T> {
     #[inline]
     fn cmp(&self, other: &Self) -> Ordering {
-        let self_chars = self.as_ref().chars().map(|c| c.to_ascii_lowercase());
-        let other_chars = other.as_ref().chars().map(|c| c.to_ascii_lowercase());
+        let self_chars = self.as_ref().bytes().map(|c| c.to_ascii_lowercase());
+        let other_chars = other.as_ref().bytes().map(|c| c.to_ascii_lowercase());
         self_chars.cmp(other_chars)
     }
 }
